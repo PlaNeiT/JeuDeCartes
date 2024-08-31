03c9c98925e7ae8d0bee264ea476d2d30e02a592
leop <leopetit200@gmail.com>
2024-08-31 17:57:05 +0200

    feature: PlayingCard -> Value, color, flag to flip the card, and a tostring

---
+++ src/model/PlayingCard.java
@@ -0,0 +1,35 @@
+package model;
+
+public class PlayingCard {
+    private final Rank rank;
+    private final Suit suit;
+    private boolean faceUp;
+
+    public PlayingCard(Rank rank, Suit suit) {
+        super();
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+
+    public boolean flip() {
+        faceUp = !faceUp;
+        return faceUp;
+    }
+
+    @Override
+    public String toString() {
+        return rank.getName() + " of " + suit.getName();
+    }
+}
